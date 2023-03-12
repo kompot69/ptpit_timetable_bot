85becc2d8534088d3dce4175cac436807f6e40a9
Kompot <69313786+kompot69@users.noreply.github.com>
2023-03-13 01:02:43 +0300

    Update bot.py

--- 0.7/bot.py
+++ 0.7/bot.py
@@ -173,7 +173,9 @@ def echo(update, context):
     # выбор группы
     #set_user(chatid)
     #if set_user:
-    gr0up = set_user(chatid)[0]
+    
+    gr0up = set_user(chatid)
+    gr0up = gr0up[0]
     print(f"(test) set group {gr0up}")
     if gr0up == None:
         text="Ваш ID не найден в боте. Установите группу:\nБот группа <имя группы>"
